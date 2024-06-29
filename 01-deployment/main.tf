4330cfb90eeaded094100ae41145232781ec02d4
Rasheed <ar.sh.dej@gmail.com>
2024-06-29 17:41:59 +0100

    chore: update deployment

--- 01-deployment/main.tf
+++ 01-deployment/main.tf
@@ -1,30 +1,44 @@
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
+
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-01b1be742d950fb7f"
   instance_type = var.instance_type
   
-  key_name = var.key_name
+  # key_name = var.key_name
   
   security_groups = ["web-traffic"]
+  # security_groups = [aws_security_group.default.id]
+
 
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
-              sudo yum install nginx -y
+              sudo yum install nginx git -y
               sudo systemctl start nginx
               sudo systemctl enable nginx
 
+              # sudo ufw allow 'Nginx Full'
+
+              mkdir -p tmp
+              mkdir -p tmp/static-page
+              mkdir -p tmp/static-page/01
+
               # Clone the static website repository from Git
-              git clone https://github.com/arshdej/hng-11.git /tmp/static-page/01
+              rm -rf tmp/static-page/01
+
+              git clone https://github.com/arshdej/hng-11.git tmp/static-page/01
 
               # Copy the website files to the NGINX web directory
-              sudo cp -r /tmp/static-page/01/* /usr/share/nginx/html/
+              sudo cp -r tmp/static-page/01/01-static-website/* /usr/share/nginx/html/
+              # sudo rm /etc/nginx/sites-enabled/default
 
               # Clean up temporary files
-              rm -rf /tmp/static-page/01
+              # rm -rf tmp/static-page/01
               EOF
   
   tags = {
@@ -56,4 +70,8 @@ resource "aws_security_group" "web_traffic" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "website_url" {
+  value = "http://${aws_instance.web.public_ip}/index.html"
 }
--- 01-deployment/variables.tf
+++ 01-deployment/variables.tf
@@ -3,14 +3,24 @@ variable "key_name" {
   type        = string
 }
 
+variable "access_key" {
+  description = "The access key from aws credential"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The secret key from aws credential"
+  type        = string
+}
+
 variable "region" {
   description = "AWS region to deploy the resources"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-north-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
